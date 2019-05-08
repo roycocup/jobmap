3bdb3484de211e8e81d779c3fc709f8931a150f8
Rodrigo Dias <rodrigo.pitta@gmail.com>
2019-05-08 21:31:06 +0100

    adding .env

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from googleplaces import GooglePlaces, types, lang
+from os import getenv
+from os.path import join, dirname
+from dotenv import load_dotenv
+ 
+# Create .env file path.
+dotenv_path = join(dirname(__file__), '.env')
+ 
+# Load file from the path.
+load_dotenv(dotenv_path)
 
-YOUR_API_KEY = ''
+YOUR_API_KEY = getenv('API_KEY')
 
 google_places = GooglePlaces(YOUR_API_KEY)
 
