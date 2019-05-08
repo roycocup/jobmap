ec8362e1d80beaa64baccd3e957c01eeda081e95
Rodrigo Dias <rodrigo.pitta@gmail.com>
2019-05-08 22:05:39 +0100

    strting bs4 but no luck

--- second.py
+++ second.py
@@ -1,2 +1,36 @@
-import requests 
+import requests
+import re
+from bs4 import BeautifulSoup
 
+
+content = None
+
+# get urls from file
+with open('output.txt', 'r') as f:
+    content = f.read()
+
+regex = 'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+
+websites = re.findall(regex, content)
+
+
+for website in websites:
+    try:
+        website = website.replace('http', 'https', 1)
+        print("trying " + website)
+        resp = requests.get(website, verify=False)
+        html_doc = resp.content()
+        print(html_doc)
+        quit()
+        # soup = BeautifulSoup(html_doc, 'html.parser')
+        # links = soup.findall('a')
+        # print(links)
+        # quit()
+    except:
+        print("something wrong with that last one")
+    
+
+
+
+# response = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+# response.status_code
